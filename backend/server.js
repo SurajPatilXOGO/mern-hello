07d8809e7c4e7b0523b836d868b84bfa94b3213b
SurajPatilXOGO <surajpatilxogo@gmail.com>
2025-03-21 14:58:29 +0530

    change on env

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 
 app.use("/api/message", messageRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
